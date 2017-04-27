9c18f145d58c6b64289f3a42f1d0af7eb7004456
DevinKamer <kamergeeksquad@gmail.com>
2017-04-27 13:50:40 -0500

    Initial Commit

---
+++ Player.py
@@ -0,0 +1,39 @@
+#Player.py
+#Devin Kamer
+#4/27/17
+
+import pygame
+import os
+
+img = pygame.image.load(os.getcwd() + "/Images/walk1.png")
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, x=0, y=0, image=img):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = image
+        self.image.set_colorkey((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self, x, y):
+        self.rect.x += x
+        self.rect.y += y
+
+    def getImage(self):
+        return self.image
+    
+    def setImage(self, image):
+        self.image = image
+        x = self.rect.x
+        y = self.rect.y
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def getPos(self):
+        return (self.rect.x, self.rect.y)
+
+        
---
+++ main.py
@@ -0,0 +1,84 @@
+#main.py
+#Devin Kamer
+#4/27/17
+
+
+import pygame
+import Player
+import os
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (1200, 700)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("Main Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Main variables
+cwd = os.getcwd()
+img = pygame.image.load(cwd + "/Images/walk1.png").convert()
+img.set_colorkey(BLACK)
+player = Player.Player(0, 0, img)
+xCur = 0
+yCur = 0
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                xCur = -2
+            elif event.key == pygame.K_RIGHT:
+                xCur = 2
+            elif event.key == pygame.K_UP:
+                yCur = -2
+            elif event.key == pygame.K_DOWN:
+                yCur = 2
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                xCur = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                yCur = 0
+
+    player.update(xCur, yCur)
+ 
+    # --- Game logic should go here
+ 
+    # --- Screen-clearing code goes here
+ 
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(WHITE)
+    screen.blit(player.getImage(), player.getPos())
+ 
+    # --- Drawing code should go here
+ 
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
